933315a72789e99a5282f08e6517bc19f92246e0
Eric Hao <erichao2018@gmail.com>
2023-10-28 15:46:46 -0400

    basic homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+import { Typography, Tabs } from 'antd';
+
+const { Title } = Typography;
+const { TabPane } = Tabs;
+
 function App() {
+  const [activeTab, setActiveTab] = useState('survey');
+
+  const handleTabChange = (key) => {
+    setActiveTab(key);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: '20px', padding: '20px', border: '1px solid #ddd', borderRadius: '8px' }}>
+      <Title style={{ textAlign: 'center', fontFamily: 'Montserrat', marginBottom: '20px' }}>brottful</Title>
+      <Tabs activeKey={activeTab} onChange={handleTabChange}>
+        <TabPane tab="Survey" key="survey">
+          <div className="content" style={{ padding: '20px', background: '#fff' }}>
+            Survey content goes here.
+          </div>
+        </TabPane>
+        <TabPane tab="Results" key="results">
+          <div className="content" style={{ padding: '20px', background: '#fff' }}>
+            Results content goes here.
+          </div>
+        </TabPane>
+      </Tabs>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
